52de77c07206ee25ab9d65147a886fafb7f417d9
reddymukkara12-svg <reddymukkara12@gmail.com>
2026-04-13 22:18:46 +0530

    update

--- app.py
+++ app.py
@@ -21,6 +21,5 @@ def submit():
 def show(id):
     return render_template("show.html", id=id)
 
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=5000)
+
+app.run(host="0.0.0.0", port=8000)
