6174381ab00149e9bd72315ca1a8111fab3de31b
reddymukkara12-svg <reddymukkara12@gmail.com>
2026-04-13 21:15:44 +0530

    update

--- app.py
+++ app.py
@@ -2,24 +2,23 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Home page
 @app.route('/')
 def index():
     return render_template("index.html")
 
-# Quote form page
+# Page for form
 @app.route('/quote')
 def quote():
     return render_template("quote.html")
 
-# Form submit
+# Handle form data
 @app.route('/submit', methods=['POST'])
 def submit():
-    name = request.form.get('name')
-    budget = request.form.get('budget')
+    name = request.form['name']
+    budget = request.form['budget']
     return f"Hello {name}, your budget is {budget}"
-
-# Show images page
 @app.route('/show/<int:id>')
 def show(id):
     return render_template("show.html", id=id)
+
+app.run(debug=True)
